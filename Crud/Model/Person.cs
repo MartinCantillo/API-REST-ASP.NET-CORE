2b400d1d602f434bd0a1759b2830faa4d138a511
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-02-16 14:01:07 -0500

    Required

--- Crud/Model/Person.cs
+++ Crud/Model/Person.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Crud.Model
 {
     class Person
     {
         private int Id { get; set; }
+
+        [Required] //indicar que el campo es requerido
         private String Nombre { get; set; }
+        
         private String Apellido { get; set; }
 
         private String Carrera { get; set; }
