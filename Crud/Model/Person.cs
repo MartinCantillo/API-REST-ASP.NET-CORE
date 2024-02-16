e4b1dd38a4d58f361b4b81f7e1bd436ddf67ad56
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-02-16 13:59:17 -0500

    model

---
+++ Crud/Model/Person.cs
@@ -0,0 +1,12 @@
+namespace Crud.Model
+{
+    class Person
+    {
+        private int Id { get; set; }
+        private String Nombre { get; set; }
+        private String Apellido { get; set; }
+
+        private String Carrera { get; set; }
+
+    }
+}
--- Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
+++ Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff60fb0b5702606731451cf9e64919bbf0735460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be57aaf06ee6f4e7911af821a14825de4fa4ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
